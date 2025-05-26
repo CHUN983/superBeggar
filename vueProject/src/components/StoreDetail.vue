31e2fb1085ef9c28cede1b0d77e4d6e11522dd5c
chh-shih <shin.0812.main@gmail.com>
2025-05-27 02:16:35 +0800

    詳細頁面分類伸縮功能補全

--- vueProject/src/components/StoreDetail.vue
+++ vueProject/src/components/StoreDetail.vue
@@ -7,9 +7,15 @@
       <p>{{ detail.address }}</p>
       <p>電話：{{ detail.tel }}</p>
       <div v-for="category in detail.info" :key="category.code">
-        <h3>{{ category.name }}（{{ category.qty }}）</h3>
-        <ul>
-          <li v-for="product in category.categories?.flatMap(c => c.products)" :key="product.code">
+        <h3 @click="toggleCategory(category.code)" class="category-title">
+          {{ category.name }}（{{ category.qty }}）
+          <span class="toggle-icon">{{ expandedCategories.includes(category.code) ? '▲' : '▼' }}</span>
+        </h3>
+        <ul v-if="expandedCategories.includes(category.code)">
+          <li
+            v-for="product in category.categories?.flatMap(c => c.products)"
+            :key="product.code"
+          >
             {{ product.name }} x {{ product.qty }}
           </li>
         </ul>
@@ -21,10 +27,19 @@
       <h2>711 - {{ detail.rawItems.StoreName }}店</h2>
       <p>{{ detail.address }}</p>
       <p>電話：{{ detail.tel }}</p>
-      <div v-for="category in detail.rawItems.CategoryStockItems" :key="category.NodeID">
-        <h3>{{ category.Name }}（{{ category.RemainingQty }}）</h3>
-        <ul>
-          <li v-for="product in category.ItemList" :key="product.ItemName">
+      <div
+        v-for="category in detail.rawItems.CategoryStockItems"
+        :key="category.NodeID"
+      >
+        <h3 @click="toggleCategory(category.NodeID)" class="category-title">
+          {{ category.Name }}（{{ category.RemainingQty }}）
+          <span class="toggle-icon">{{ expandedCategories.includes(category.NodeID) ? '▲' : '▼' }}</span>
+        </h3>
+        <ul v-if="expandedCategories.includes(category.NodeID)">
+          <li
+            v-for="product in category.ItemList"
+            :key="product.ItemName"
+          >
             {{ product.ItemName }} x {{ product.RemainingQty }}
           </li>
         </ul>
@@ -32,6 +47,7 @@
     </template>
   </div>
 </template>
+
 
 <script setup>
 import { ref } from 'vue'
